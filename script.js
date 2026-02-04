51de4f500bf85f0f8f5042315bec542d98793288
saeleuterio <saeleuterio@hotmail.com>
2026-02-04 20:24:48 -0300

    Inicio

---
+++ script.js
@@ -0,0 +1,27 @@
+// Esse é um comentário no código! não será interpretado pelo JS
+
+
+// Formas diferentes de criar variáveis
+// 1 - variável global - somente o nome da variável
+// 2 - var - modo antigo de criar uma variável
+// 3 - let - modo atual de criar uma variável
+// 4 - const - valores que são constantes e não mudam
+
+
+// Essa é uma variável que guarda o valor da minha idade atual
+let idade = 49
+
+// A data que eu faço aniversario
+const dataAniversario = '2 de janeiro'
+
+// Imprimindo a minha idade atula no console
+console.log('Minha idade atual:', idade)
+
+// Imprimindo a minha data de aniversário no console
+console.log('Data de aniversário:', dataAniversario)
+
+// Idade depois que fiz aniversário
+idade = 50
+
+// Imprimindo minha idade atual novamente
+console.log('Minha idade depois do aniversário:', idade)
