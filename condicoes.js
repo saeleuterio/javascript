376b9b6e016f89b3c266c75361fdd23018c1393c
saeleuterio <saeleuterio@hotmail.com>
2026-02-13 17:37:04 -0300

    up

---
+++ condicoes.js
@@ -0,0 +1,17 @@
+const nome = 'Saulo'
+
+if (nome) {
+    console.log('Olá,', nome)
+} else {
+    console.log('Ainda não sei seu nome')
+}
+
+const idade = null
+
+if (idade != null) {
+    if (idade >= 18) {
+      console.log("Maior de idade");
+    } else if (idade >= 0 && idade < 18) {
+      console.log("Menor de idade");
+    }
+}
