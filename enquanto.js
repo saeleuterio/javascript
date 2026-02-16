4394883c9bdf878bbb5f21ef543b6e2851cfba6f
saeleuterio <saeleuterio@hotmail.com>
2026-02-16 18:04:52 -0300

    up

---
+++ enquanto.js
@@ -0,0 +1,13 @@
+let contador = 1
+
+while(contador <= 10) {
+    console.log('Número atual:', contador)
+    contador++
+}
+
+let numero = 1
+
+do {
+    console.log('Número:', numero)
+    numero++
+} while (numero <= 10)
