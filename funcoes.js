fe40544789123ec85021de5e4d4cf02bd17c0765
saeleuterio <saeleuterio@hotmail.com>
2026-02-23 19:48:54 -0300

    up

--- funcoes.js
+++ funcoes.js
@@ -1,14 +1,28 @@
-function saudacao(nome) {// Nome é um parametro da função saudação
-  console.log("Olá,", nome);
-}
+// function saudacao(nome) { // Nome é um parametro da função saudação
+//   console.log("Olá,", nome);
+// }
+
+// const saudacao = (nome) => {
+//     console.log('Vida longa e próspera,', nome)
+// }
+
+const saudacao = nome => console.log('Olá,', nome)
 
 saudacao("Saulo"); // Saulo é o "argumento"
 saudacao("Kelly");
 saudacao("Giulia");
 
-function calcularDobroDeUm(numero) {
-  return numero * 2;
-}
+// function calcularDobroDeUm(numero) {
+//   return numero * 2;
+// }
+
+// const calcularDobroDeUm = (numero) => {
+//     return numero * 2
+// }
+
+const calcularDobroDeUm = numero => numero * 2
 
 const numeroDobrado = calcularDobroDeUm(4);
 console.log("O dobro de 4 é:", numeroDobrado);
+
+console.log("oi", "tudo", "bem", "?");
