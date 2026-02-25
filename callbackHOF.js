f1057fea26e0a17fc6718c7d8ac6393d58b52504
saeleuterio <saeleuterio@hotmail.com>
2026-02-25 07:49:21 -0300

    up

---
+++ callbackHOF.js
@@ -0,0 +1,19 @@
+// HOF -> Higher Ordem Function === Função que recebe outra função como parametro
+
+function calcular(numero1, numero2, operacao) {
+  return operacao(numero1, numero2);
+}
+
+function soma(num1, num2) {
+  return num1 + num2;
+}
+
+function divisao(num1, num2) {
+  return num1 / num2;
+}
+
+const resultadoSoma = calcular(3, 8, soma); // Soma é um callback
+console.log('Resultado da soma:', resultadoSoma);
+
+const resultadoDivisao = calcular(32, 8, divisao); // Divisão é um callback
+console.log('Resultado da divisão:', resultadoDivisao);
