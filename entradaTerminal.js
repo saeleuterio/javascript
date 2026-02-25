a5c3908b39d5ca01c152ae489aa1149c8f4e13ea
saeleuterio <saeleuterio@hotmail.com>
2026-02-25 16:35:46 -0300

    up

--- entradaTerminal.js
+++ entradaTerminal.js
@@ -1,23 +1,21 @@
-const readline = require('readline')
+const readline = require("readline");
 
 const leitor = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
-})
+  output: process.stdout,
+});
 
-leitor.question('Qual é o seu nome? ', (nome) => {
-  console.log('Olá,', nome)
-  console.log('Boa vindas ao nosso sistema!')
+leitor.question("Qual é o seu nome? ", (nome) => {
+  console.log("Olá,", nome);
+  console.log("Boa vindas ao nosso sistema!");
 
-  leitor.question('Qual é a sua idade? ', (idade) => {
-
+  leitor.question("Qual é a sua idade? ", (idade) => {
     if (idade > 18) {
-        console.log('Uau! Você pode tirar a sua CNH!')
+      console.log("Uau! Você pode tirar a sua CNH!");
     } else {
-        console.log('Você não pode tirar a sua CNH!')
+      console.log("Você não pode tirar a sua CNH!");
     }
 
-    leitor.close()
-  })
-
-})
+    leitor.close();
+  });
+});
