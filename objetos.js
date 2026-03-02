e484889b11062047cdd82402d4cb12a6e5223163
saeleuterio <saeleuterio@hotmail.com>
2026-03-02 20:03:02 -0300

    up

--- objetos.js
+++ objetos.js
@@ -1,21 +1,44 @@
 const pessoa = {
-    nome: 'Ana',
-    idade: 26,
-    temCNH: true
-}
+  nome: "Ana",
+  idade: 26,
+  temCNH: true,
+};
 
-pessoa.sobrenome = 'Paula'
+pessoa.sobrenome = "Paula";
 
-console.log('Nome:', pessoa.nome)
-console.log('Sobrenome:', pessoa.sobrenome)
+// console.log('Nome:', pessoa.nome)
+// console.log('Sobrenome:', pessoa.sobrenome)
 
 const livro = {
-    titulo: 'O Hobbit',
-    autor: 'J. R. R. Tolkien',
-    paginas: 310
-}
+  titulo: "O Hobbit",
+  autor: "J. R. R. Tolkien",
+  paginas: 310,
+};
 
-livro.publicado = true
-livro.idiomas = [
-    'Ingles', 'Portugues', 'Espanhol'
-]
+livro.publicado = true;
+livro.idiomas = ["Ingles", "Portugues", "Espanhol"];
+
+livro.idiomas.push("Mandarim");
+livro.idiomas.push("Francês");
+
+delete livro.paginas;
+
+console.log("Livro antes:", livro);
+console.log("Livro depois:", livro);
+
+console.log("Autor do livro:", livro["autor"]);
+console.log("Editora:", livro["editora"]);
+
+const autor = {
+  nome: "J R R Tolkien",
+  nacionalidade: "Britanico",
+  idade: 98,
+  livros: [livro],
+};
+
+livro.autor = autor;
+
+console.log("Autor", autor);
+
+livro.autor.nome;
+livro.autor.nacionalidade;
