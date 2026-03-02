f02f0461df6b9d95b4bd9e2aa82155eb59d9c549
saeleuterio <saeleuterio@hotmail.com>
2026-03-02 19:16:12 -0300

    up

---
+++ objetos.js
@@ -0,0 +1,21 @@
+const pessoa = {
+    nome: 'Ana',
+    idade: 26,
+    temCNH: true
+}
+
+pessoa.sobrenome = 'Paula'
+
+console.log('Nome:', pessoa.nome)
+console.log('Sobrenome:', pessoa.sobrenome)
+
+const livro = {
+    titulo: 'O Hobbit',
+    autor: 'J. R. R. Tolkien',
+    paginas: 310
+}
+
+livro.publicado = true
+livro.idiomas = [
+    'Ingles', 'Portugues', 'Espanhol'
+]
